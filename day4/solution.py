5977f20781c4b14c56c912820f8c1734f3687c1d
Vadim Patsev <basvasilich@yandex-team.ru>
2023-12-05 00:20:23 +0400

    day4

--- day4/solution.py
+++ day4/solution.py
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2023/day/2
+# https://adventofcode.com/2023/day/4
 import os
 import re
 
@@ -15,7 +15,7 @@ def read_input(filename: str) -> list[(int, list[(int, int, int)])]:
         line = raw_line.rstrip()
         card_prefix, card_data = line.split(':')
         card_id = int(re.sub(r'\D', '', card_prefix))
-        card_numbers = card_data.split('|')
+        card_numbers = re.sub(r'\s+', ' ', card_data).split('|')
         winning_numbers = card_numbers[0].strip().split(' ')
         my_numbers = card_numbers[1].strip().split(' ')
         result.append((card_id, winning_numbers, my_numbers))
@@ -38,8 +38,26 @@ def solution_part1(filename: str) -> int:
     return result
 
 
-# assert (solution_part2('data/input.test.txt') == 467835)
-# print('Result Part 2: ', solution_part2('data/adventofcode.com_2023_day_3_input.txt'))
+def solution_part2(filename: str) -> int:
+    cards = read_input(filename)
+    card_copies = {}
+    for card_id, _, _ in cards:
+        card_copies[card_id] = 1
+
+    for card_id, winning_numbers, my_numbers in cards:
+        count = 0
+        for num in winning_numbers:
+            if num in my_numbers:
+                count += 1
+        if count > 0:
+            for next_card_id in range(card_id + 1, card_id + count + 1):
+                card_copies[next_card_id] += card_copies[card_id]
+
+    return sum(card_copies.values())
+
+
+assert (solution_part2('data/input.test.txt') == 30)
+print('Result Part 2: ', solution_part2('data/adventofcode.com_2023_day_4_input.txt'))
 
 assert (solution_part1('data/input.test.txt') == 13)
 print('Result Part 1: ', solution_part1('data/adventofcode.com_2023_day_4_input.txt'))
